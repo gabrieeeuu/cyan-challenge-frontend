435a1935b5d6c2518eb61d668831eae21c7148a7
gabrieeeuu <gabriel.almeida@ccc.ufcg.edu.br>
2020-08-26 07:31:52 -0300

    api url added

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { getToken } from "./Auth";
 
 const api = axios.create({
-  baseURL: "https://cyan-challenge-server.herokuapp.com//"
+  baseURL: "https://cyan-challenge-server.herokuapp.com/"
 });
 
 api.interceptors.request.use(async config => {
