f241db445d3baf98f26c088ce9e3f78803d030ac
Gabriel Guimaraẽs de Almeida <gguimaraes.2712@gmail.com>
2020-08-26 07:59:07 -0300

    Server API url added

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { getToken } from "./Auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/"
+  baseURL: "https://cyan-challenge-server.herokuapp.com/"
 });
 
 api.interceptors.request.use(async config => {
